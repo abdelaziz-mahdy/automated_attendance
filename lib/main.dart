a1259e4d19440012a68d8efc91e8dcbf8c091fc9
Abdelaziz Mahdy <zezohassam@gmail.com>
2025-01-05 18:34:06 -0400

    Add camera model and service for camera management; refactor imports and update asset paths and add models

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:cameras_viewer/camera_grid_view.dart';
-import 'package:cameras_viewer/camera_model.dart';
-import 'package:cameras_viewer/camera_service.dart';
+import 'package:cameras_viewer/views/camera_grid_view.dart';
+import 'package:cameras_viewer/models/camera_model.dart';
+import 'package:cameras_viewer/services/camera_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
